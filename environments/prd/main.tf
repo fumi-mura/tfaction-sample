07ce78a5efbbc9a618df94b692edbeae24d25723
fumi-mura <381704fumi@gmail.com>
2025-07-21 17:48:07 +0900

    fix

--- environments/prd/main.tf
+++ environments/prd/main.tf
@@ -15,19 +15,15 @@ module "init" {
   }
 }
 
-module "test_cwlogs1" {
-  source      = "../../modules/cloudwatch/logs"
-  cwlogs_name = "test-cwlogs1"
-}
+# module "test_cwlogs1" {
+#   source      = "../../modules/cloudwatch/logs"
+#   cwlogs_name = "test-cwlogs1"
+# }
 
 # module "test_cwlogs2" {
 #   source = "../../modules/cloudwatch/logs"
 #   cwlogs_name = "test-cwlogs2"
 # }
-
-resource "aws_cloudwatch_log_group" "test_cwlogs1" {
-  name = "test-cwlogs1"
-}
 
 import {
   id = "test-cwlogs1"
