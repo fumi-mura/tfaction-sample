e18be792bebf0a947f9d11d3eeea87815f56c582
fumi-mura <381704fumi@gmail.com>
2025-10-06 02:12:11 +0900

    feat: Delete unuse files

--- environments/dev/providers.tf
+++
@@ -1,9 +0,0 @@
-# provider "aws" {
-#   default_tags {
-#     tags = {
-#       Environment        = local.env
-#       ServiceName        = local.ServiceName
-#       ManagedByTerraform = true
-#     }
-#   }
-# }
