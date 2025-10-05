9a4b715a76e0fb728d86e40463e166f8b824df5e
fumi-mura <381704fumi@gmail.com>
2025-10-06 02:11:32 +0900

    feat: Migrate dir

---
+++ environments/dev/init/main.tf
@@ -0,0 +1,29 @@
+data "aws_caller_identity" "current" {}
+
+module "init" {
+  source     = "../../../modules/init"
+  env        = local.env
+  account_id = data.aws_caller_identity.current.account_id
+  iam_roles = {
+    plan_admin = {
+      exec           = "plan"
+      policy         = "readonly"
+      managed_policy = "ReadOnlyAccess"
+    }
+    apply_admin = {
+      exec           = "apply"
+      policy         = "admin"
+      managed_policy = "AdministratorAccess"
+    }
+  }
+}
+
+module "cwlogs1" {
+  source      = "../../../modules/cloudwatch/logs"
+  cwlogs_name = "test1"
+}
+
+module "cwlogs2" {
+  source      = "../../../modules/cloudwatch/logs"
+  cwlogs_name = "test2"
+}
